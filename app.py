2ca7ca3e3b38b7f03c0ee0449d988bff4624dcf9
samgabre <131766063+samgabre@users.noreply.github.com>
2024-05-09 12:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app=Flask(__name__)
+
+@app.route("/home")
+def home():
+  return "welcome"
+
+@app.route("/coffee")
+def coffee():
+  return "coffee"
+
+if __name__=="__main__:
+  app.run()
